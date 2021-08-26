437ed56d832b5938f216be8a44f4c1808fdbb0f1
kyleremil <kyleremil@gmail.com>
2021-08-25 22:58:42 -0400

    first commit

--- current_challange/nested_lists.py
+++ current_challange/nested_lists.py
@@ -1,4 +1,4 @@
-raw_input_list = []
+students = []
 input_file = open(r"input.txt")
 
 
@@ -8,10 +8,15 @@ def raw_input():
 
 def create_raw_input_list():
     # if __name__ == '__main__':
+    second_lowest = []
     for _ in range(int(raw_input())):
         student_name_and_score = []
         name = raw_input()
         score = float(raw_input())
         student_name_and_score.append(name)
         student_name_and_score.append(score)
-        raw_input_list.append(student_name_and_score)
+        students.append(student_name_and_score)
+        if len(students > 1):
+            for student in students:
+
+create_raw_input_list()
--- current_challange/nested_lists_test.py
+++ current_challange/nested_lists_test.py
@@ -2,5 +2,20 @@ import functools
 import nested_lists as nl
 
 input_file = open(r"input.txt")
+input_list = input_file.readlines()
 
-assert functools.reduce(lambda a, b: a and b, map(lambda x, y: x == y, nl.raw_input_list, input_file.readlines()))
+
+
+def test_raw_input_list():
+    nl.raw_input_list
+    # raw_input_list = []
+    # for _ in range(int(nl.raw_input())):
+    #     # temp_input_list = []
+    #     name = nl.raw_input()
+    #     raw_input_list.append(name)
+    #     score = float(nl.raw_input())
+    #     raw_input_list.append(score)
+    # print(functools.reduce(lambda a, b: a and b, map(lambda x, y: x == y, raw_input_list, input_list)))
+
+
+test_raw_input_list()
