e4af259c64da359cae71ca8c0588e8fdb8148882
Kyleremil <kyleremil@gmail.com>
2021-08-29 21:11:20 -0400

    redid nexted lists

--- current_challange/nested_lists.py
+++ current_challange/nested_lists.py
@@ -4,23 +4,20 @@ def raw_input():
     return input_file.readline()
 
 
-def create_raw_input_list():
+def main():
     # if __name__ == '__main__':
     students = []
     second_lowests = []
-    counter = 0
+    counter = 1
 
     name = raw_input()
     score = float(raw_input())
     students.append([name, score])
     if counter > 1:
-        for student in students:
-            for counter in len(students):
-                if student[1] > students[counter + 1][1]:
+        for counter in len(students):
+                # if student[1] > students[counter + 1][1]:
+            print(counter)
 
     counter +=1
 
-
-
-
-create_raw_input_list()
+main()
